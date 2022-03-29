8c8847305544f1fd45860b173a1fd7677a903722
凌天 <49418651+lingtian152@users.noreply.github.com>
2022-03-28 21:08:01 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,33 +1,33 @@
 import asyncio
+from asyncio.windows_events import NULL
 import json
+import random
+import aiohttp
+import requests
+from pathlib import Path
 
-
-from tkinter.ttk import Style
 from graia.broadcast import Broadcast
 from graia.ariadne.app import Ariadne
 from graia.ariadne.event.message import GroupMessage, FriendMessage
 from graia.ariadne.message.chain import MessageChain
-from graia.ariadne.message.element import Plain
+from graia.ariadne.message.element import Plain, Image
 from graia.ariadne.model import Group, MiraiSession, Friend, Member
-from graia.ariadne.message.parser.twilight import Twilight,ParamMatch, Sparkle
+from graia.ariadne.message.parser.twilight import Twilight, ParamMatch, Sparkle, MatchResult
 from graia.ariadne.console import Console
-from graia.saya import Saya
 from graia.broadcast import Broadcast
 from graia.saya.builtins.broadcast import BroadcastBehaviour
+from graia.ariadne import get_running
+from graia.ariadne.adapter import Adapter
 
 
-
-
-
-
-loop = asyncio.get_event_loop()
+loop = asyncio.new_event_loop()
 
 broadcast = Broadcast(loop=loop)
 app = Ariadne(
     broadcast=broadcast,
     connect_info=MiraiSession(
         host="http://localhost:8080",  # 填入 HTTP API 服务运行的地址
-        verify_key="INITKEYH6TIQjMz",  # 填入 verifyKey
+        verify_key="INITKEY5Fqqtvij",  # 填入 verifyKey
         account=1330542948,  # 你的机器人的 qq 号
     )
 )
@@ -36,53 +36,51 @@ app = Ariadne(
 con = Console(broadcast=broadcast, prompt="Console> ")
 
 
-loop = asyncio.get_event_loop()
-broadcast = Broadcast(loop=loop)
-
-
-
 @con.register()
 async def cmd(app: Ariadne, command: str):
     if str(command) == "stop":
         await app.stop()
 
 
-
 @broadcast.receiver("GroupMessage")
-async def group_message_listener(app: Ariadne, group: Group, message: MessageChain, arg=None):
-    if str(message) == "早上好":
-        await app.sendGroupMessage(group, MessageChain.create([Plain("早")]))
-    elif str(message) == "早":
-        await app.sendGroupMessage(group, MessageChain.create([Plain("早")]))
-    elif str(message) == "早啊":
-        await app.sendGroupMessage(group, MessageChain.create([Plain("早")]))
-    
-    if Member.id == int(1553396053):
-        if str(message) == ';关闭':
-            with open("./group.json", "r") as f:
-                prefix = json.load(f)
-
-            prefix[str(Member.group.id)] = str("关闭")
-            
-            with open("./group.json", "w") as f:
-                json.dump(prefix, f, indent=4)
-
+async def group_message_listener(app: Ariadne, group: Group, msg: MessageChain):
+    if group.id != int(435237486):
+        return NULL
+    else:
+        if str(msg) == "菜单":
+            await app.sendGroupMessage(group, msg.create([Plain(f'菜单 \n ----------- \n 1. 看腿 \n 2. 二次元(暂时无法使用) \n 3. 看美女(看妹子) \n -----------')]))
+        elif str(msg) == "看腿":
+            session = get_running(Adapter).session
+            async with session.get("http://api.lingfeng.press/api/tk.php") as r:
+                data = await r.read()
+                await app.sendGroupMessage(group, msg.create(Image(data_bytes=data)))
+        elif str(msg) == "看妹子" or str(msg) == "看美女":
+            session = get_running(Adapter).session
+            img = ["http://api.lingfeng.press/api/pcmnt.php",
+                   "http://api.lingfeng.press/api/sjmnt.php",
+                   "http://api.lingfeng.press/api/sjmnt2.php"]
+            async with session.get(str(random.choice(img))) as r:
+                data = await r.read()
+                await app.sendGroupMessage(group, msg.create(Image(data_bytes=data)))
 
 
 @broadcast.receiver("FriendMessage")
-async def on_message(app: Ariadne, friend: Friend, group: Group, msg: MessageChain):
-    if friend.id == int(1553396053):
-        if str(msg) == ";关机":
-            await app.sendFriendMessage(friend, msg.create([Plain("机器人已关机")]))
-            await app.stop()
-        elif str(msg) == ";stop":
-            await app.sendFriendMessage(friend, msg.create([Plain("机器人已关机")]))
-            await app.stop()
-        elif str(msg) == ";菜单":
-            await app.sendFriendMessage(friend, msg.create([Plain(f'菜单 \n ----------- \n 1. ;版本 \n 2. ;关机 \n -----------')]))
-        elif str(msg) == ";版本":
-            await app.sendFriendMessage(friend, msg.create([Plain(f'当前版本: {str(app.remote_version)}')]))
-        else:
-            await app.sendFriendMessage(friend, msg.create([Plain("未查询到当前指令, 请使用 ;帮助 查看指令")]))
+async def on_message(app: Ariadne, friend: Friend, msg: MessageChain):
+    if str(msg) == "菜单":
+        await app.sendFriendMessage(friend, msg.create([Plain(f'菜单 \n ----------- \n 1. 看腿 \n 2. 二次元(暂时无法使用) \n 3. 看美女(看妹子) \n -----------')]))
+    elif str(msg) == "看腿":
+        session = get_running(Adapter).session
+        async with session.get("http://api.lingfeng.press/api/tk.php") as r:
+            data = await r.read()
+            await app.sendFriendMessage(friend, msg.create(Image(data_bytes=data)))
+    elif str(msg) == "看妹子" or str(msg) == "看美女":
+        session = get_running(Adapter).session
+        img = ["http://api.lingfeng.press/api/pcmnt.php",
+               "http://api.lingfeng.press/api/sjmnt.php",
+               "http://api.lingfeng.press/api/sjmnt2.php"]
+        async with session.get(str(random.choice(img))) as r:
+            data = await r.read()
+            await app.sendFriendMessage(friend, msg.create(Image(data_bytes=data)))
+
 
 loop.run_until_complete(app.lifecycle())
